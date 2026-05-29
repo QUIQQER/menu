4aeb168d3620e25d0e28a0c5353e45b74faeaab9
Henning <leutz@pcsg.de>
2026-05-29 11:12:33 +0200

    fix: show loader while lazy slideout loads

--- bin/SlideOutLazy.js
+++ bin/SlideOutLazy.js
@@ -23,6 +23,7 @@ define('package/quiqqer/menu/bin/SlideOutLazy', [
 
             this.$Control = null;
             this.$Loading = null;
+            this.$loaderNodes = [];
             this.$menuButtonDisabled = false;
         },
 
@@ -40,7 +41,7 @@ define('package/quiqqer/menu/bin/SlideOutLazy', [
                 return this.$Loading;
             }
 
-            this.$Loading = new Promise((resolve) => {
+            this.$Loading = new Promise((resolve, reject) => {
                 require([this.getAttribute('control')], (SlideOutControl) => {
                     const Control = new SlideOutControl(this.getAttributes());
 
@@ -56,7 +57,7 @@ define('package/quiqqer/menu/bin/SlideOutLazy', [
 
                     this.$Control = Control;
                     resolve(Control);
-                });
+                }, reject);
             });
 
             return this.$Loading;
@@ -68,11 +69,88 @@ define('package/quiqqer/menu/bin/SlideOutLazy', [
          * @return {Promise}
          */
         toggle: function () {
+            if (this.$Control) {
+                this.$Control.toggle();
+                return Promise.resolve(this.$Control);
+            }
+
+            if (this.$Loading) {
+                return this.$Loading;
+            }
+
+            this.$showLoader();
+
             return this.$load().then((Control) => {
+                this.$hideLoader();
                 Control.toggle();
 
                 return Control;
+            }).catch((error) => {
+                this.$Loading = null;
+                this.$hideLoader();
+                throw error;
             });
+        },
+
+        /**
+         * Show a lightweight loader on configured menu buttons while SlideOut is loading.
+         */
+        $showLoader: function () {
+            const buttons = this.$getMenuButtons();
+
+            buttons.forEach((Button) => {
+                if (Button.get('data-slideout-lazy-loading') === '1') {
+                    return;
+                }
+
+                const Loader = new Element('span', {
+                    'class': 'fa fa-spinner fa-spin quiqqer-menu-slideoutLazy-loader',
+                    'aria-hidden': 'true',
+                    styles: {
+                        marginLeft: 8
+                    }
+                }).inject(Button);
+
+                Button.set('data-slideout-lazy-loading', '1');
+                Button.set('aria-busy', 'true');
+                Button.addClass('quiqqer-menu-slideoutLazy-loading');
+
+                this.$loaderNodes.push({
+                    Button: Button,
+                    Loader: Loader
+                });
+            });
+        },
+
+        /**
+         * Remove loader nodes from menu buttons.
+         */
+        $hideLoader: function () {
+            this.$loaderNodes.forEach((Entry) => {
+                Entry.Button.erase('data-slideout-lazy-loading');
+                Entry.Button.erase('aria-busy');
+                Entry.Button.removeClass('quiqqer-menu-slideoutLazy-loading');
+                Entry.Loader.destroy();
+            });
+
+            this.$loaderNodes = [];
+        },
+
+        /**
+         * Return configured menu button elements.
+         *
+         * @return {Array}
+         */
+        $getMenuButtons: function () {
+            const buttonIds = this.getAttribute('buttonids');
+
+            if (!buttonIds) {
+                return [];
+            }
+
+            return buttonIds.split(',')
+                .map((buttonId) => document.id(buttonId.trim()))
+                .filter((Button) => !!Button);
         },
 
         /**
